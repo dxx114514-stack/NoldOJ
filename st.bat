02863671c34648b410cf476329fc308bc741e255
dxx114514-stack <2703729270@qq.com>
2026-07-31 20:00:07 +0800

    自动提交 周五 2026/07/31 20:00:07.24

--- st.bat
+++ st.bat
@@ -1,6 +1,6 @@
 @echo off
 md log
-start "" /B "D:/cfd/cloudflared.exe" tunnel run winoj > log\tunnel.log 2>&1
+start "" /B "cloudflared.exe" tunnel run winoj > log\tunnel.log 2>&1
 
 start "" /B "start.bat"
 
